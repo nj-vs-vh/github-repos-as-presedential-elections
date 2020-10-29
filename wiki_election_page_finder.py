150b0f5bc692be452a0475b102c980a2555e6144
Igor Vaiman <gosha.vaiman@gmail.com>
2020-10-30 02:26:58 +0300

    Refined election refs search

--- wiki_election_page_finder.py
+++ wiki_election_page_finder.py
@@ -10,18 +10,20 @@ from tqdm import trange
 WIKI_URL = 'https://en.wikipedia.org/'
 
 
-DATA_FOLDER = 'data'
+DATA_FOLDER = './data'
 
 elections_filename = DATA_FOLDER + '/election_wiki_pages.txt'
 
 
 class WikiElectionRefsParser(HTMLParser):
     elections_regex = re.compile(r'presidential[ _]election', flags=re.IGNORECASE)
-    year_regex = re.compile(r'\d{4,4}')
 
     def __init__(self):
-        with open(elections_filename, 'r') as f:
-            self.elections = {line.strip('\n') for line in f if line}
+        try:
+            with open(elections_filename, 'r') as f:
+                self.elections = {line.strip('\n') for line in f if line}
+        except FileNotFoundError:
+            self.elections = set()
         super().__init__()
 
     def handle_starttag(self, tag, attrs):
@@ -30,7 +32,7 @@ class WikiElectionRefsParser(HTMLParser):
         attrs_dict = {k: v for k, v in attrs}
         href = attrs_dict.get('href', '')
         title = attrs_dict.get('title', '')
-        if self.elections_regex.search(href + title) and self.year_regex.search(href + title):
+        if self.elections_regex.search(href) or self.elections_regex.search(title):
             self.elections.add(href)
 
     def dump(self):
